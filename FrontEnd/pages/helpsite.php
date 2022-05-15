1afc70f1e21fc7c3885c8ed342d9f50269a2dcb6
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-05-15 22:07:29 +0200

    Hepl page and Imprint Page

---
+++ FrontEnd/pages/helpsite.php
@@ -0,0 +1,34 @@
+<?php
+    include '../webstructure/head.php';
+?>
+    <title>Help page</title>
+</head>
+<body>
+  <?php
+    include '../webstructure/menuleiste.php';
+  ?>
+<div style="background-image: url('./img/background.jpg'); min-height:100%;">
+    <br><!--A little bit of information concerning the website.-->
+    <div class="container text-center col-6 offset-3 border border-primary alert-secondary rounded"><br>
+        <h1 id = "heading-1">Help & Support </h1>
+        <br><br>
+        <h3> User instructions </h3>
+        <br><br>
+        <p>
+            <a>At imprint you can find out more about our Service</a><br>
+        </p>
+    
+        <div>
+            <p>You can also contact us under:</p>
+        </div>
+        <div>
+            <p> Tel.: +43 680 676 598 <br> hilfseite@dreamcoffin.at </p>
+        </div>
+    </div>
+    <br>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ FrontEnd/pages/imprint.php
@@ -0,0 +1,37 @@
+<?php
+    include '../webstructure/head.php';
+?>
+    <title>Imprint</title>
+</head>
+<body>
+  <?php
+    include '../webstructure/menuleiste.php';
+  ?>
+
+
+<div style="background-image: url('../res/img/background.jpg');">
+<br><!--Imprint of the hotel-->
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-8 offset-2 border border-primary alert-secondary rounded">
+          <h1 class="page-header">Imprint <br> <small> Dream Coffin </small></h1>
+            <img src="../res/img/Libertas_urn.jpg" class="rounded mx-auto d-block" alt="Urn_photo" width="200">
+              <p class="text-center">
+                <br>
+                  Dream Coffin GmbH<br>
+                  Dresdnerstrasse 85, A- 1200 Wien<br><br>
+                  Tel.: +43 680 676 598<br>
+                  Fax: +43 680 895 755<br>
+                  E-Mail: office@dreamcoffin.com<br>
+                  Web: www.dream_coffin.com<br><br>
+                  Commercial Register number: 325 854s<br>
+                  Commercial Register Court: Gerichtsstand Wien<br>
+                  Regulatory authority: Magistrat der Stadt Wien<br>
+                  Membership: WKO, ÖHV<br>
+                  UID: ATU12345678<br>
+            </p>
+          </div>
+        </div>
+      </div><br>
+</div>
+    
