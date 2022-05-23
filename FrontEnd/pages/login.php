1d88983a106db7dd913109686b305c8fb16a566c
NadiiaGoncharenko <83373973+NadiiaGoncharenko@users.noreply.github.com>
2022-05-23 12:38:47 +0200

    Login

--- FrontEnd/pages/login.php
+++ FrontEnd/pages/login.php
@@ -2,42 +2,40 @@
     include '../webstructure/head.php';
   ?>
     <title>Login</title>
+    
 </head>
-
 <body>
 
-<!-- damit html wird auch gelesen/hab kaput gemacht -->
-<!-- <div w3-include-html="../webstructure/menuleiste.php"> </div>  -->
- 
 <?php
     include '../webstructure/menuleiste.php';
-  ?>
+?>
 
 <div style="background-image: url('../res/img/background.jpg'); padding-bottom:1%">
-<br> <?php $loginErr = ""; ?>
-
+    <br> 
+    <?php $loginErr = ""; ?>
     <div class="container">
         <!--Das Login Formular-->
         <?php if(!isset($_SESSION["userID"])): ?>
             <h1 class=" page-header text-light">Login</h1>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <form id="login_form"  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                 <div class="form-group">
                     <label class="text-light" for="username">Username</label>
-                    <input type="text" id="username" name="username" class="form-control"><br>
+                    <input type="text" id="username_log" name="username" class="form-control"><br>
                     <label class="text-light" for="password">Password</label>
-                    <input type="password" id="password" name="password" class="form-control">
+                    <input type="password" id="password_log" name="password" class="form-control">
                     <label>
                     <input type="checkbox" name="angemeldet_bleiben" value="1"> Angemeldet bleiben</label><br>
                     
                 </div>
                 <!-- <span class="error"><#?php echo $loginErr;?></span> -->
                 <br>
-                <button type="submit" id="login" class="btn btn-primary">Login</button>
+                <!-- <button type="submit" id="login" class="btn btn-primary">Login</button> -->
+                <input type="button" name="login" class="btn btn-primary" onclick="../js/loginAjax.js" value="Login" id="login">
             </form>
         <!--Die Anzeige für eingeloggte User zum Ausloggen-->
         <?php else:?>
             <h2 class="text-light">Hello, <?php echo $_SESSION["username"]?>!</h2>
-                <div class="text-light">
+                <div class="text-light" id="logout">
                     Push the button "Logout" to logout from your profil.
                 </div><br>
             <a class="btn btn-primary" href="?logout=true">Logout</a>
--- FrontEnd/webstructure/head.php
+++ FrontEnd/webstructure/head.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registration</title>
-	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.js"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">    
     <link rel="stylesheet" href="../res/css/style.css">
@@ -10,3 +9,4 @@
 	<script src="../js/ajaxsuche.js"></script>
 	<script src="../js/ajaxFkt.js"></script>
 	<script src="../js/fkt.js"></script>
+	<script src="../js/loginAjax.js"></script>
